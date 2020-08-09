cff495c12137b439680a55b031cdb4c5b0507547
georges dagher <georgesdagher98@hotmail.com>
2020-08-09 10:39:04 +0300

    test

---
+++ app/build/generated/source/buildConfig/debug/com/company/testapp/BuildConfig.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a008ad32cb93198ebbe60024678801b023478f771008c6afce7eaba4e0c422c9
+size 445
---
+++ app/build/kotlin/compileDebugKotlin/caches-jvm/lookups/lookups.tab.values.s
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f81ac88fb689751b2053d7559859d61e8951ca8df36f4dfc8ca4601902d2b23
+size 2
